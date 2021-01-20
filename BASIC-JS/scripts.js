87e213ff281e04a2c74bf5f27d05ce4b96d06ba5
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-01-20 21:09:19 +0200

    task completed

--- BASIC-JS/scripts.js
+++ BASIC-JS/scripts.js
@@ -6,23 +6,27 @@ if(Math.floor(Math.random()*10)<5 ) {number2 = 0 } else number2 = 1
 if(Math.floor(Math.random()*10)<5 ) {number3 = 0 } else number3 = 1
 if(Math.floor(Math.random()*10)<5 ) {number4 = 0 } else number4 = 1
 if(Math.floor(Math.random()*10)<5 ) {number5 = 0 } else number5 = 1
+let probability = (1/2**5)*100
+
 console.log( 'your random numbers :', number1, number2, number3, number4,number5)
+console.log('chance of winning a Jackpot is ' + probability + ' %' )
+
 let numberString = ''+number1+number2+number3+number4+number5
-console.log(numberString)
+
 
 if(numberString=== '11111'){
     console.log("congratulation!! You have won the Jackpot!!")
+
 }
 else if(numberString==='00000'){
-    console.log(' Goodluck for next time, Congrats for getting all 0')
+    console.log('Congrats for getting all 0, Goodluck for next time to win the Jackpot')
 }
 else{
-    let probability = (1/2**5)*100
-    console.log('Thanks for participating! its hard to win a jackpot always which has probablity only ' + probability + ' %')
+    console.log('Thanks for participating! its hard to win a Jackpot always which has probablity only ' + probability + ' %  but not impossible. Play Again!' )
 }
 
 
-
+/*
 // using switch statement
 switch(numberString){
     case '11111':
@@ -35,3 +39,4 @@ switch(numberString){
         let probability = (1/2**5)*100
         console.log('Thanks for participating! its hard to win a jackpot always which has probablity only ' + probability + ' %')
     }
+*/
